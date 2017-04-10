462a69eba03e375be7382fbcd1ff749302198001
Mark Raymond <blading82@gmail.com>
2017-04-10 09:56:16 -0400

    renabled outlook desktop application

--- morning_coffee.bat
+++ morning_coffee.bat
@@ -13,8 +13,8 @@ cd "C:\Program Files (x86)\Google\Chrome\Application\"
 start chrome.exe --profile-directory="Profile 1"
 
 rem Outlook
-rem cd "C:\Program Files (x86)\Microsoft Office\root\Office16\"
-rem start outlook.exe
+cd "C:\Program Files (x86)\Microsoft Office\root\Office16\"
+start outlook.exe
 
 rem Skype for Business (Lync)
 cd "C:\Program Files (x86)\Microsoft Office\root\Office16\"
